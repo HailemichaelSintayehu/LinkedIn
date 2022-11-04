244c1b68ac75bedcb890db36b6daac8efef2d4fd
HailemichaelSintayehu <hmichaelsintayehu@gmail.com>
2022-11-04 13:00:33 +0300

    header left finished

--- src/Header.js
+++ src/Header.js
@@ -1,24 +1,24 @@
 import React from 'react'
 import SearchIcon from '@mui/icons-material/Search';
+import "./Header.css"
 function Header() {
   return (
     <div className ='header'>
-       <h1>This is a Header</h1>
 
        <div className = "header_left">
 
-            <img className = "image_linkedin" src = "https://www.maryville.edu/wp-content/uploads/2015/11/Linkedin-logo-1-550x550-300x300.png" alt = ""/>
+            <img src = "https://www.maryville.edu/wp-content/uploads/2015/11/Linkedin-logo-1-550x550-300x300.png" alt = ""/>
 
             <div className="header_search">
                 <SearchIcon/>
                 
-                <input type = "text" placeholder = "search here"/>
+                <input type = "text" placeholder = "Search"/>
 
             </div>
        </div>
 
        <div className = "header_right">
-                right
+                {/* right */}
        </div>
 
     </div>
