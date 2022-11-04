0b8e8511dfa56f8025a6b17003aa495002eb5ffe
HailemichaelSintayehu <hmichaelsintayehu@gmail.com>
2022-11-04 13:33:34 +0300

    header right added

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import SearchIcon from '@mui/icons-material/Search';
 import "./Header.css"
+import HeaderOption from './HeaderOption';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
 function Header() {
   return (
     <div className ='header'>
@@ -18,7 +21,12 @@ function Header() {
        </div>
 
        <div className = "header_right">
-                {/* right */}
+            <HeaderOption Icon = {HomeIcon}  title = "Home"/>
+            <HeaderOption Icon = {SupervisorAccountIcon}  title = "My Network"/>
+            <HeaderOption  title = "Jobs"/>
+            <HeaderOption  title = "Messaging"/>
+            <HeaderOption  title = "Notifications"/>
+            
        </div>
 
     </div>
