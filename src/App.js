c9529abe9c7c4b6be3da7bc413971aad81c16d5a
HailemichaelSintayehu <hmichaelsintayehu@gmail.com>
2022-11-09 12:16:35 +0300

    widget css added using material ui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Widgets } from '@mui/icons-material';
+
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
@@ -8,6 +8,7 @@ import { auth } from './firebase';
 import Header from './Header';
 import Login from './Login';
 import Sidebar from './Sidebar';
+import Widgets from './Widgets';
 
 function App() {
   const user = useSelector(selectUser)
--- src/Widgets.js
+++ src/Widgets.js
@@ -1,12 +1,34 @@
 import React from 'react'
 import "./Widget.css"
+import InfoIcon from "@material-ui/icons/Info"
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 function Widgets() {
+
+  const newArticle = (heading ,subtitle) =>(
+
+    <div className="widgets_article">
+      <div className="widget_articleLeft">
+        <FiberManualRecordIcon/>
+      </div>
+      <div className="widget_articleRight">
+        <h4>{heading}</h4>
+        <p>{subtitle}</p>
+      </div>
+    </div>
+  )
+  
   return (
     <div className = "widget">
         <div className="widgets_header">
             <h2>Linkedin News</h2>
             <InfoIcon/>
         </div>
+            {newArticle("kest misael is back","Top news -9099 readers")}
+            {newArticle("Coronavirus","Top news -884 readers")}
+            {newArticle("Tesla hits new highs","Top news -9099 readers")}
+            {newArticle("Bitcoin Breaks $22k","Crypto -8000 readers")}
+            {newArticle("SpaceX launches new Eagle rocket","Top news -6503 readers")}
+
     </div>
   )
 }
